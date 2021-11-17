007a46686bb2c8a061d2bb0b6502a1224b7d5c24
Gabriel Resende <gabresendemkt@gmail.com>
2021-11-17 18:18:31 -0300

    exercicios pré aula ao vivo

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-trabalhando-com-elementos/exercicios-pre-aula/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-trabalhando-com-elementos/exercicios-pre-aula/script.js
@@ -13,3 +13,26 @@
 // // 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
 // // 8 - Agora acesse o terceiroFilho a partir de pai .
 // document.querySelector('#pai').lastElementChild.previousElementSibling
+
+// let pai = document.querySelector('#pai');
+
+// let irmaoDeOndeEstou = document.createElement('section');
+
+// pai.appendChild(irmaoDeOndeEstou);
+
+// Buscar elemento pai
+var elemento_pai = document.body;
+
+// Criar elemento
+var titulo = document.createElement('h1');
+titulo.className = 'classe-h1'
+
+
+// Criar o nó de texto
+var texto = document.createTextNode("Um título qualquer");
+
+// Anexar o nó de texto ao elemento h1
+titulo.appendChild(texto);
+
+// Agora sim, inserir (anexar) o elemento filho (titulo) ao elemento pai (body)
+paiDoPai.appendChild(titulo);
