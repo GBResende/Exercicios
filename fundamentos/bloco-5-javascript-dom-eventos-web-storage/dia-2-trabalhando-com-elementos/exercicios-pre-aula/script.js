b5c86a9261e5d01fdc6b5154511a2569b07bf0ba
Gabriel Resende <gabresendemkt@gmail.com>
2021-11-17 15:37:05 -0300

    primeira parte dos exercicios pre aula

---
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-2-trabalhando-com-elementos/exercicios-pre-aula/script.js
@@ -0,0 +1,15 @@
+// // 1 - Acesse o elemento elementoOndeVoceEsta .
+// document.querySelector('#pai').firstElementChild.nextElementSibling
+// // 2 - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'red'
+// // 3 - Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// document.querySelector('#primeiroFilhoDoFilho').innerText = 'Adicionando um Texto ao elemento PrimeiroFilhoDoFilho'
+// // 4 - Acesse o primeiroFilho a partir de pai .
+// document.querySelector('#pai').firstElementChild
+// // 5 - Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+// document.querySelector('#elementoOndeVoceEsta').previousElementSibling
+// // 6 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+// document.querySelector('#elementoOndeVoceEsta').nextSibling
+// // 7 - Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+// // 8 - Agora acesse o terceiroFilho a partir de pai .
+// document.querySelector('#pai').lastElementChild.previousElementSibling
